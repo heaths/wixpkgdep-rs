818893dd9d300bee095a11fbd6cc01023684bcf2
Heath Stewart <heaths@outlook.com>
2023-11-12 13:53:27 -0800

    Implement check_dependents (#1)

---
+++ examples/chkdeps.rs
@@ -0,0 +1,42 @@
+// Copyright 2023 Heath Stewart.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+use clap::Parser;
+use std::error::Error;
+use wixpkgdep;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    let dependents = wixpkgdep::check_dependents(
+        args.provider_key,
+        args.scope,
+        Default::default(),
+        &args.ignore,
+    )?;
+
+    for d in dependents.iter() {
+        println!("{d}");
+    }
+
+    if dependents.len() > 0 {
+        std::process::exit(1);
+    }
+
+    Ok(())
+}
+
+#[derive(Parser)]
+#[command(author, version, about = "Gets dependents of a provider key.")]
+struct Args {
+    /// The provider key to check for dependents.
+    #[arg(short = 'k', long, value_name = "KEY")]
+    provider_key: String,
+
+    /// The scope under which to check for dependents.
+    #[arg(long, value_parser, default_value_t)]
+    scope: wixpkgdep::Scope,
+
+    /// Dependents to ignore.
+    #[arg(long, value_name = "DEPENDENCIES")]
+    ignore: Option<Vec<String>>,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,143 @@
-use windows::core::w;
+// Copyright 2023 Heath Stewart.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+use std::{fmt::Display, str::FromStr};
+
+use windows::{
+    core::{w, PCWSTR},
+    Win32::System::Registry::HKEY,
+};
 
 mod registry;
 
-pub fn get_program_files_dir() -> Option<String> {
-    let key = registry::Key::open(
-        registry::HKEY_LOCAL_MACHINE,
-        w!("SOFTWARE\\Microsoft\\Windows\\CurrentVersion"),
-    )
-    .ok()?;
-    if let Some(registry::Value::String(s)) = key.value(w!("ProgramFilesDir")) {
-        return Some(s);
-    }
-
-    None
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum Scope {
+    User,
+    Machine,
 }
 
-pub fn enum_installers() {
-    let key = registry::Key::open(
-        registry::HKEY_CURRENT_USER,
-        w!("Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall"),
+#[derive(Copy, Clone, Debug, PartialEq)]
+#[repr(C)]
+pub enum Attributes {
+    None,
+    MinVersionInclusive = 0x100,
+    MaxVersionInclusive = 0x200,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Error {
+    NotFound,
+    NotSupported,
+    RegistryError(windows::core::Error),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub fn check_dependents<K>(
+    provider_key: K,
+    scope: Scope,
+    _attributes: Attributes,
+    ignore: &Option<Vec<K>>,
+) -> Result<Vec<String>>
+where
+    K: AsRef<str> + PartialEq,
+{
+    let key = registry::Key::open::<HKEY, PCWSTR>(
+        scope.into(),
+        w!("Software\\Classes\\Installer\\Dependencies"),
     )
-    .unwrap();
-    for k in key.keys().unwrap() {
-        for v in k.values().unwrap() {
-            match v {
-                registry::Value::Binary(data) => println!("{:?}", data),
-                registry::Value::DWord(num) => println!("{}", num),
-                registry::Value::MultiString(arr) => println!("{:?}", arr),
-                registry::Value::QWord(num) => println!("{}", num),
-                registry::Value::String(s) => println!("{}", s),
+    .map_err(map_windows_error)?;
+
+    let provider_key: Vec<u16> = provider_key
+        .as_ref()
+        .encode_utf16()
+        .chain(std::iter::once(0))
+        .collect();
+    let provider_key: PCWSTR = PCWSTR::from_raw(provider_key.as_ptr());
+    let key = key
+        .open_subkey::<PCWSTR>(provider_key)
+        .map_err(map_windows_error)?;
+
+    let key = key
+        .open_subkey::<PCWSTR>(w!("Dependents"))
+        .map_err(map_windows_error)?;
+
+    Ok(key
+        .keys()?
+        .into_iter()
+        .filter_map(|k| unsafe {
+            if let Some(ignore) = ignore {
+                if ignore.contains(std::mem::transmute(&k.name)) {
+                    return None;
+                }
             }
+            Some(k.name.clone())
+        })
+        .collect())
+}
+
+impl Default for Scope {
+    fn default() -> Self {
+        Scope::Machine
+    }
+}
+
+impl Default for Attributes {
+    fn default() -> Self {
+        Attributes::None
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::NotFound => write!(f, "not found"),
+            Error::NotSupported => write!(f, "not supported"),
+            Error::RegistryError(err) => write!(f, "{}", err),
         }
+    }
+}
+
+impl Display for Scope {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Scope::User => write!(f, "user"),
+            Scope::Machine => write!(f, "machine"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl From<windows::core::Error> for Error {
+    fn from(value: windows::core::Error) -> Self {
+        Error::RegistryError(value)
+    }
+}
+
+impl FromStr for Scope {
+    type Err = Error;
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "user" => Ok(Scope::User),
+            "machine" => Ok(Scope::Machine),
+            _ => Err(Error::NotSupported),
+        }
+    }
+}
+
+impl Into<windows::Win32::System::Registry::HKEY> for Scope {
+    fn into(self) -> windows::Win32::System::Registry::HKEY {
+        match self {
+            Scope::User => registry::HKEY_CURRENT_USER,
+            Scope::Machine => registry::HKEY_LOCAL_MACHINE,
+        }
+    }
+}
+
+fn map_windows_error(err: windows::core::Error) -> Error {
+    match err.code() {
+        registry::E_FILE_NOT_FOUND => Error::NotFound,
+        _ => Error::RegistryError(err),
     }
 }
