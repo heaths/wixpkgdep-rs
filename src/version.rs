b43c7ef383b532fd7d91eb457ad2047708423921
Heath Stewart <heaths@microsoft.com>
2023-11-14 19:46:17 -0800

    More efficient Version

--- src/version.rs
+++ src/version.rs
@@ -2,36 +2,31 @@
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 use crate::Error;
-use std::cmp::Ordering;
 use std::fmt::Display;
 
 /// A comparable version containing major.minor.build.revision fields.
-#[derive(Debug, Default, Clone, Copy, PartialEq)]
-pub struct Version {
-    major: u16,
-    minor: u16,
-    build: u16,
-    revision: u16,
-}
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Version(u64);
 
-impl PartialOrd for Version {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        match self.major.cmp(&other.major) {
-            Ordering::Equal => {}
-            ord => return Some(ord),
-        }
+impl Version {
+    /// Gets the major version number.
+    pub fn major(&self) -> u16 {
+        (self.0 >> 48) as u16
+    }
 
-        match self.minor.cmp(&other.minor) {
-            Ordering::Equal => {}
-            ord => return Some(ord),
-        }
+    /// Gets the minor version number.
+    pub fn minor(&self) -> u16 {
+        (self.0 >> 32) as u16
+    }
 
-        match self.build.cmp(&other.build) {
-            Ordering::Equal => {}
-            ord => return Some(ord),
-        }
+    /// Gets the build version number.
+    pub fn build(&self) -> u16 {
+        (self.0 >> 16) as u16
+    }
 
-        Some(self.revision.cmp(&other.revision))
+    /// Gets the revision version number.
+    pub fn revision(&self) -> u16 {
+        self.0 as u16
     }
 }
 
@@ -40,39 +35,34 @@ impl Display for Version {
         write!(
             f,
             "{}.{}.{}.{}",
-            self.major, self.minor, self.build, self.revision
+            self.major(),
+            self.minor(),
+            self.build(),
+            self.revision()
         )
     }
 }
 
 impl From<[u16; 4]> for Version {
     fn from(value: [u16; 4]) -> Self {
-        Version {
-            major: value[0],
-            minor: value[1],
-            build: value[2],
-            revision: value[3],
-        }
+        Version(
+            (value[0] as u64) << 48
+                | (value[1] as u64) << 32
+                | (value[2] as u64) << 16
+                | value[3] as u64,
+        )
     }
 }
 
 impl From<u64> for Version {
     fn from(value: u64) -> Self {
-        Version {
-            major: ((value >> 48) as u16),
-            minor: ((value >> 32) as u16),
-            build: ((value >> 16) as u16),
-            revision: (value as u16),
-        }
+        Version(value)
     }
 }
 
 impl From<Version> for u64 {
     fn from(value: Version) -> u64 {
-        (value.major as u64) << 48
-            | (value.minor as u64) << 32
-            | (value.build as u64) << 16
-            | value.revision as u64
+        value.0
     }
 }
 
@@ -81,7 +71,6 @@ impl TryFrom<String> for Version {
 
     fn try_from(value: String) -> Result<Self, Self::Error> {
         let value = value.trim_start_matches(|c| c == 'v' || c == 'V');
-
         let mut fields = [0u16; 4];
 
         for (i, part) in value.split('.').enumerate() {
@@ -170,6 +159,18 @@ mod tests {
     }
 
     #[test]
+    fn version_try_from_prefix_string_ok() {
+        assert_eq!(
+            Version::try_from("v1.2.3.4".to_string()).unwrap(),
+            Version::from([1, 2, 3, 4])
+        );
+        assert_eq!(
+            Version::try_from("V1.2.3.4".to_string()).unwrap(),
+            Version::from([1, 2, 3, 4])
+        );
+    }
+
+    #[test]
     fn version_try_from_string_err_format() {
         assert_eq!(
             Version::try_from("test".to_string()).unwrap_err(),
@@ -183,5 +184,14 @@ mod tests {
             Version::try_from("1.2.3.4.5".to_string()).unwrap_err(),
             Error::Format
         );
+    }
+
+    #[test]
+    fn version_properties() {
+        let version = Version::from([1, 2, 3, 4]);
+        assert_eq!(version.major(), 1);
+        assert_eq!(version.minor(), 2);
+        assert_eq!(version.build(), 3);
+        assert_eq!(version.revision(), 4);
     }
 }
